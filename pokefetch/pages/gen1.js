6d620787f12deb524ff45e3cb648da4be551bb54
wjahorn <william.familyhorn@btinternet.com>
2024-07-19 10:52:53 +0100

    Update gen1.js

--- pokefetch/pages/gen1.js
+++ pokefetch/pages/gen1.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 export const getStaticProps = async () => {
@@ -11,6 +12,13 @@ export const getStaticProps = async () => {
 };
 
 const HomePage = ({ pokemon }) => {
+  const [clickedPokemonData, setClickedPokemonData] = useState(null);
+  const handleClick = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setClickedPokemonData(data);
+    }
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="bg-yellow-300 text-red-600 text-lg font-bold italic text-center py-4">
@@ -18,21 +26,50 @@ const HomePage = ({ pokemon }) => {
       </h1>
       <div className="bg-red-600 text-blue-500 text-center space-x-60 p-8 text-xl font-semibold">
         <button className="bg-yellow-300 p-4 rounded-full hover:text-blue-700">
-            <Link href="/">Back to Homepage</Link>
+          <Link href="/">Back to Homepage</Link>
         </button>
         <button className="bg-yellow-300 p-4 rounded-full hover:text-blue-700">
-            <Link href="/gen2">Generation 2</Link>
+          <Link href="/gen2">Generation 2</Link>
         </button>
       </div>
       <ul className="grid grid-cols-4 gap-4 mt-4 capitalize">
-        {pokemon.slice(0, 151).map((mon) => (
+        {pokemon.slice(0,151).map((mon) => (
           <li
             key={mon.name}
             className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow"
           >
-            <a className="text-blue-500 hover:text-blue-700">
+            <a
+              className="text-blue-500 hover:text-blue-700 cursor-pointer"
+              onClick={() => handleClick(mon.url)}
+            >
               <h3 className="text-xl font-semibold">{mon.name}</h3>
             </a>
+            {clickedPokemonData && clickedPokemonData.name === mon.name && (
+              <div className="mt-2 p-2 rounded-lg">
+                <Image 
+                src={clickedPokemonData.sprites.front_default} 
+                width={192} 
+                height={192}
+                className="mx-auto">
+                </Image>
+                {clickedPokemonData.types.map((typeinfo) => (
+                  <li
+                    key={typeinfo.type.name}
+                  >
+                    <p>Type: {typeinfo.type.name}</p>
+                  </li>
+                ))}
+                {clickedPokemonData.stats.map((statinfo) => (
+                  <li
+                    key={statinfo.stat.name}
+                  >
+                    <p>{statinfo.stat.name}: {statinfo.base_stat}</p>
+                  </li>
+                ))}
+                <p>Height: {clickedPokemonData.height}</p>
+                <p>Weight: {clickedPokemonData.weight}</p>
+              </div>
+            )}
           </li>
         ))}
       </ul>
