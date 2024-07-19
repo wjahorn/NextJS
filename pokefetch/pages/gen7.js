fcd74858f1c4b817179473e0a498baa00c50b2f0
wjahorn <william.familyhorn@btinternet.com>
2024-07-19 10:56:44 +0100

    Update gen7.js

--- pokefetch/pages/gen7.js
+++ pokefetch/pages/gen7.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 export const getStaticProps = async () => {
@@ -11,6 +12,13 @@ export const getStaticProps = async () => {
 };
 
 const HomePage = ({ pokemon }) => {
+  const [clickedPokemonData, setClickedPokemonData] = useState(null);
+  const handleClick = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setClickedPokemonData(data);
+    }
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="bg-yellow-300 text-red-600 text-lg font-bold italic text-center py-4">
@@ -32,15 +40,44 @@ const HomePage = ({ pokemon }) => {
           <li
             key={mon.name}
             className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow"
+        >
+          <a
+            className="text-blue-500 hover:text-blue-700 cursor-pointer"
+            onClick={() => handleClick(mon.url)}
           >
-            <a className="text-blue-500 hover:text-blue-700">
-              <h3 className="text-xl font-semibold">{mon.name}</h3>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <h3 className="text-xl font-semibold">{mon.name}</h3>
+          </a>
+          {clickedPokemonData && clickedPokemonData.name === mon.name && (
+            <div className="mt-2 p-2 rounded-lg">
+              <Image 
+              src={clickedPokemonData.sprites.front_default} 
+              width={192} 
+              height={192}
+              className="mx-auto">
+              </Image>
+              {clickedPokemonData.types.map((typeinfo) => (
+                <li
+                  key={typeinfo.type.name}
+                >
+                  <p>Type: {typeinfo.type.name}</p>
+                </li>
+              ))}
+              {clickedPokemonData.stats.map((statinfo) => (
+                <li
+                  key={statinfo.stat.name}
+                >
+                  <p>{statinfo.stat.name}: {statinfo.base_stat}</p>
+                </li>
+              ))}
+              <p>Height: {clickedPokemonData.height}</p>
+              <p>Weight: {clickedPokemonData.weight}</p>
+            </div>
+          )}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 };
 
 export default HomePage;
